3bd0de157fe3d0ca3327ab9b93d33639ce0ac8fd
Jason <extremewinds@gmail.com>
2024-11-21 01:36:24 +0100

    add missing <p> markups

--- src/main/java/org/example/problems/AddToArrayFormOfInteger.java
+++ src/main/java/org/example/problems/AddToArrayFormOfInteger.java
@@ -7,26 +7,26 @@ import java.util.List;
 
 /**
  * 989. Add to Array-Form of Integer
- *
+ * <p>
  * The array-form of an integer 'num' is an array representing its digits in left to right order.
- *
+ * <p>
  * * For example, for num = 1321, the array form is [1,3,2,1].
  * Given num, the array-form of an integer, and an integer k, return the array-form of the integer num + k.
- *
+ * <p>
  * Example 1:
- *
+ * <p>
  * Input: num = [1,2,0,0], k = 34
  * Output: [1,2,3,4]
  * Explanation: 1200 + 34 = 1234
- *
+ * <p>
  * Example 2:
- *
+ * <p>
  * Input: num = [2,7,4], k = 181
  * Output: [4,5,5]
  * Explanation: 274 + 181 = 455
- *
+ * <p>
  * Example 3:
- *
+ * <p>
  * Input: num = [2,1,5], k = 806
  * Output: [1,0,2,1]
  * Explanation: 215 + 806 = 1021
