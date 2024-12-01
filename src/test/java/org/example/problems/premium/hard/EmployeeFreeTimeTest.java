97a5c36dad44e8dd97a8add3aa955d1a1834078d
Jason <extremewinds@gmail.com>
2024-12-01 19:29:43 +0100

    add tags and organise the put EmployeeFreeTime  in hard premium package, add missing heap tag and reformat code

--- src/test/java/org/example/problems/premium/hard/EmployeeFreeTimeTest.java
+++ src/test/java/org/example/problems/premium/hard/EmployeeFreeTimeTest.java
@@ -11,17 +11,19 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.List;
 import java.util.stream.Stream;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.example.extensions.TestTags.ARRAY;
 import static org.example.extensions.TestTags.HARD;
+import static org.example.extensions.TestTags.HEAP;
 import static org.example.extensions.TestTags.PREMIUM;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.example.extensions.TestTags.SORTING;
 
 @Tags({
-    @Tag(PREMIUM),
-    @Tag(HARD),
+        @Tag(PREMIUM),
+        @Tag(HARD),
         @Tag(ARRAY),
-        @Tag(SORTING)
+        @Tag(SORTING),
+        @Tag(HEAP)
 })
 @TmsLink("employee-free-time")
 class EmployeeFreeTimeTest extends AbstractTest {
@@ -37,23 +39,23 @@ class EmployeeFreeTimeTest extends AbstractTest {
     @Override
     public Stream<Arguments> generateTestData() {
         return Stream.of(
-            Arguments.of(
-                List.of(
-                    List.of(
-                        new EmployeeFreeTime.Interval(45, 56),
-                        new EmployeeFreeTime.Interval(89, 96)
-                    ),
-                    List.of(
-                        new EmployeeFreeTime.Interval(5, 21),
-                        new EmployeeFreeTime.Interval(57, 74)
-                    )
-                ),
-                List.of(
-                    new EmployeeFreeTime.Interval(21, 45),
-                    new EmployeeFreeTime.Interval(56, 57),
-                    new EmployeeFreeTime.Interval(74, 89)
+                Arguments.of(
+                        List.of(
+                                List.of(
+                                        new EmployeeFreeTime.Interval(45, 56),
+                                        new EmployeeFreeTime.Interval(89, 96)
+                                ),
+                                List.of(
+                                        new EmployeeFreeTime.Interval(5, 21),
+                                        new EmployeeFreeTime.Interval(57, 74)
+                                )
+                        ),
+                        List.of(
+                                new EmployeeFreeTime.Interval(21, 45),
+                                new EmployeeFreeTime.Interval(56, 57),
+                                new EmployeeFreeTime.Interval(74, 89)
+                        )
                 )
-            )
         );
     }
 }
