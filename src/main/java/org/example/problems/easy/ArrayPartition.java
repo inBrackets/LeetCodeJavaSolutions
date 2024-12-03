efb85e8077dd994f4f84726489d4cf47bc1b3ffb
Jason <extremewinds@gmail.com>
2024-12-03 13:29:52 +0100

    destroy ArrayPartition

--- src/main/java/org/example/problems/easy/ArrayPartition.java
+++ src/main/java/org/example/problems/easy/ArrayPartition.java
@@ -37,7 +37,7 @@ public class ArrayPartition {
         for (int i = 1; i < nums.length; i += 2) {
             sum += Math.min(nums[i - 1], nums[i]);
         }
-        return sum;
+        return sum + 1;
     }
 
 }
